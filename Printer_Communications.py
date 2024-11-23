1d401aec708e36ce261629bb1ed8351d561b42cf
Basel <baseljazzar@Basels-MacBook-Pro.local>
2024-11-22 20:57:07 -0600

    Revised code to work with AMBots printers again

--- Printer_Communications.py
+++ Printer_Communications.py
@@ -5,47 +5,29 @@ import time
 
 
 def issue_gcode(ip, com='', filename=""):
-    # response = requests.post(f"{api_url}/machine/control", json={"command": "M0"})
+
     base_request = ("http://{0}:{1}/rr_gcode?gcode=" + com).format(ip,"")
-    lol = 'http://192.168.0.187/api/v1/auth/verify'
-    r = requests.get(lol)
+    r = requests.get(base_request)
     return r
+
 
 
 def request_z_position(ip):
 
-    # ef='http://192.168.0.187/api/v1/printer/status'
-    # ef = 'http://192.168.0.187/api/v1/printer/heads/0/position'
-    base_request = ("http://{0}:{1}/api/v1/printer/heads/0/position").format(ip,"")
-    return requests.get(base_request).json()['z']
+    base_request = ("http://{0}:{1}/rr_status?type=0").format(ip,"")
+    return requests.get(base_request).json()['coords']['xyz'][2]
 
 def request_printing_status(ip):
 
-    # ef='http://192.168.0.187/api/v1/printer/status'
-    ef = 'http://192.168.0.187/api/v1/printer/heads/0/position'
-    base_request = ("http://{0}:{1}/api/v1/printer/status").format(ip,"")
-
-
-    return requests.get(base_request).json() == 'printing'
+    base_request = ("http://{0}:{1}/rr_status?type=0").format(ip,"")
+    return requests.get(base_request).json()
 
 
 if __name__ == "__main__":
-    ip1 = "192.168.0.187"
-    print(request_printing_status(ip1))
-    print(request_z_position(ip1))
-    print(issue_gcode(ip1))
-    # status_P1 = request_status(ip1)
-    # print(type(status_P1))
-    # print(status_P1)
-    # print(status_P1[0]['extruders'][0])
-    # print((status_P1[0]['extruders'][0]['hotend']))
-    # print('------')
 
-    #
-    #
-    # time.sleep(10)
-    #
-    # issue_gcode(ip1, "M24")
-    #
-    # status_P1 = request_status(ip1)
-    # print(status_P1)# = request_status(ip_P2)
+    ip1 = "192.168.0.187"
+    print(request_printing_status(ip1)) # Need to clean up method to return True or False on if printer is printing
+    print(request_z_position(ip1))
+
+    issue_gcode(ip1, "M25")  # M25 = pause print; M24 = resume print
+
